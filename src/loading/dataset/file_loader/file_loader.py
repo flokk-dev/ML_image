acfd84980632e0807f746cd87033e32bc05e82b0
Floroux <florian.hocquet@etu.univ-amu.fr>
2023-03-13 22:23:40 +0100

    revert

--- src/loading/dataset/file_loader/file_loader.py
+++ src/loading/dataset/file_loader/file_loader.py
@@ -9,53 +9,19 @@ Purpose:
 # IMPORT: utils
 import os
 
-# IMPORT: loading
-import torch
-
 
 class FileLoader:
     def __init__(self):
-        """ Represents a general loading method, that will be derived depending on the use case. """
         self._file_extensions = None
 
-    def _verify_path(self, file_path):
-        """
-        Verifies if file_path's extension is handled by the data loader.
-
-        Args:
-            file_path (str): path to the file
-
-        Raises:
-            ValueError: the file_path's extension isn't handled by the data loader
-        """
-        if not os.path.basename(file_path).split(".")[1] in self._file_extensions:
+    def _verify_path(self, path):
+        if not os.path.basename(path).split(".")[1] in self._file_extensions:
             raise ValueError("The file extension isn't supported.")
         return True
 
-    def _load(self, file_path):
-        """
-        Loads a file into a Tensor.
-
-        Args:
-            file_path (str): path to the file
-
-        Returns:
-            torch.Tensor: file's content as a Tensor
-
-        Raises:
-            NotImplementedError: abstract method
-        """
+    def _load(self, path):
         raise NotImplementedError("Mother class.")
 
-    def __call__(self, file_path):
-        """
-        Verifies and loads a file into a Tensor.
-
-        Args:
-            file_path (str): path to the file
-
-        Returns:
-            torch.Tensor: file's content as a Tensor
-        """
-        self._verify_path(file_path)
-        return self._load(file_path)
+    def __call__(self, path):
+        self._verify_path(path)
+        return self._load(path)
--- src/loading/dataset/file_loader/file_loaders.py
+++ src/loading/dataset/file_loader/file_loaders.py
@@ -19,70 +19,40 @@ from .file_loader import FileLoader
 
 class ImageLoader(FileLoader):
     def __init__(self):
-        """ Represents the image loading method. """
         # Mother Class
         super(ImageLoader, self).__init__()
 
         # Attributes
         self._file_extensions = ["png", "jpg", "jpeg"]
 
-    def _load(self, file_path):
-        """
-        Loads an image into a Tensor.
-
-        Args:
-            file_path (str): path to the file
-
-        Returns:
-            torch.Tensor: image as a Tensor
-        """
-        array = cv2.imread(file_path, cv2.IMREAD_UNCHANGED)
+    def _load(self, path):
+        array = cv2.imread(path, cv2.IMREAD_UNCHANGED)
         return torch.from_numpy(array).type(torch.float32)
 
 
 class NpyLoader(FileLoader):
     def __init__(self):
-        """ Represents the npy file loading method. """
         # Mother Class
         super(NpyLoader, self).__init__()
 
         # Attributes
         self._file_extensions = ["npy"]
 
-    def _load(self, file_path):
-        """
-        Loads a npy file's content into a Tensor.
-
-        Args:
-            file_path (str): path to the Numpy file
-
-        Returns:
-            torch.Tensor: npy file's content as a Tensor
-        """
-        array = np.load(file_path).copy()
+    def _load(self, path):
+        array = np.load(path).copy()
         return torch.from_numpy(array).type(torch.float32)
 
 
 class NpzLoader(FileLoader):
     def __init__(self):
-        """ Represents the npz file loading method. """
         # Mother Class
         super(NpzLoader, self).__init__()
 
         # Attributes
         self._file_extensions = ["npz"]
 
-    def _load(self, file_path):
-        """
-        Loads a npz file's content into a Tensor.
-
-        Args:
-            file_path (str): file to the path
-
-        Returns:
-            torch.Tensor: npz file's content as a Tensor
-        """
-        array = np.load(file_path, allow_pickle=True)
+    def _load(self, path):
+        array = np.load(path, allow_pickle=True)
         header = array["header"][()]
 
         array = zstd.decompress(array["data"])
@@ -94,21 +64,11 @@ class NpzLoader(FileLoader):
 
 class PtLoader(FileLoader):
     def __init__(self):
-        """ Represents the pt file loading method. """
         # Mother Class
         super(PtLoader, self).__init__()
 
         # Attributes
         self._file_extensions = ["pt"]
 
-    def _load(self, file_path):
-        """
-        Loads a pt file's content into a Tensor.
-
-        Args:
-            file_path (str): path to the file
-
-        Returns:
-            torch.Tensor: pt file's content as a Tensor
-        """
-        return torch.load(file_path).type(torch.float32)
+    def _load(self, path):
+        return torch.load(path).type(torch.float32)
